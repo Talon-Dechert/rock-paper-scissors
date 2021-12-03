831f02ba702a6484f60fc3f08ad9ad3f5208e9b2
Talon Dechert <techdechert@outlook.com>
2021-12-02 21:45:29 -0800

    Go insane trying to mutate variables through scopes

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 let selection = ['Rock', 'Paper', 'Scissors'];
+let pick;
+let tie;
+let win;
+let lose;
+let error;
+let quit;
+let playerSelection;
+let computerSelection;
 
 function computerPlay() {
     let roll = (Math.floor(Math.random() * 3));
@@ -7,6 +15,9 @@ function computerPlay() {
 };
 
 function playRound(playerSelection, computerSelection) {
+    if (playerSelection === null) {
+        return(quit);
+    }
 
     playerSelection = playerSelection.toLowerCase();
 
@@ -24,51 +35,81 @@ function playRound(playerSelection, computerSelection) {
                 return(win);
                 break;
             }
-            case 'paper':
-                if (computerSelection === 'rock') {
-                    return(win);
-                    break;
-                } else if (computerSelection === 'scissors') {
-                    return(lose);
-                    break;
-                } else {
-                    return(tie);
-                    break;
-                }
-                case 'scissors':
-                    if (computerSelection === 'rock'){
-                        return(lose);
-                        break;
-                    } else if (computerSelection === 'paper') {
-                        return(win);
-                        break;
-                    } else {
-                        return(tie);
-                        break;
-                    }
-                    default:
-                        return(`Error! Try again! You entered ${playerSelection}`);
-                        break;
-                    };
+        case 'paper':
+            if (computerSelection === 'rock') {
+                return(win);
+                break;
+            } else if (computerSelection === 'scissors') {
+                return(lose);
+                break;
+            } else {
+                return(tie);
+                break;
+            }
+        case 'scissors':
+            if (computerSelection === 'rock'){
+                return(lose);
+                break;
+            } else if (computerSelection === 'paper') {
+                return(win);
+                break;
+            } else {
+                return(tie);
+                break;
+            }
+        default:
+            return(error);
+            break;
+        };
 };
 
+
+
 function game() {
+    
+    let score = 0;
+    
     for (let round = 1; round <=5; round++) {
         console.log(`Round ${round}!`);
         
-        playRound(playerSelection, computerSelection);
+        playerSelection = prompt(`Choose one!`, `Rock, Paper, Scissors`);
+        computerSelection = computerPlay();
+        
+        
+        let result = playRound(playerSelection, computerSelection);
+        
+        if (result === win){
+            score++;
+        } else if (result === lose) {
+            score--;
+        } else if (result === error){
+            round--;
+        } else if (result === quit){
+            break;
+        }
+        
+        console.log(result);
     }
 }
 
+game();
+
+pick = `You picked ${playerSelection}.
+Computer picked ${computerSelection}. \n`;
+
+tie = `${pick}It's a tie!`;
+win = `${pick}You win!`;
+lose = `${pick}You lose!`;
+error = `Error! Try again! You entered ${playerSelection}`;
+quit = `No worries! Try again another time!`;
+
+
+
 /*
 const playerSelection = `Scissors`;
-const computerSelection = computerPlay();
 
-let pick = `You picked ${playerSelection}.
-Computer picked ${computerSelection}. \n`;
-let tie = `${pick}It's a tie!`;
-let win = `${pick}You win!`;
-let lose = `${pick}You lose!`;
+
+
 
 
 console.log(playRound(playerSelection, computerSelection));
